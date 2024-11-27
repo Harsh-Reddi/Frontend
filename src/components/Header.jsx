2fea70478d9e4e414c6e224ee73de894a8882a75
Harsh-Reddi <Harshithareddi14@gmail.com>
2024-11-27 11:15:12 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -66,7 +66,7 @@ const Header = () => {
                                 </div>
                                 <div className='flex group cursor-pointer text-slate-600 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px]
                                 after:bg-[#afafaf] after:-right-[16px] after:absolute before:absolute before:h-[18px] before:bg-[#afafaf] before:w-[1px] before:-left-[20px]'>
-                                    <img src="http://localhost:3000/images/language.png" alt="" />
+                                    <img src="https://frontend-lilac-nine.vercel.app/images/language.png" alt="" />
                                     <span><IoMdArrowDropdown /></span>
                                     <ul className='absolute invisible transition-all top-12 rounded-sm duration-200 text-white
                                     p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10'>
@@ -95,7 +95,7 @@ const Header = () => {
                         <div className='md-lg:w-full w-3/12 md-lg:pt-4'>
                             <div className='flex justify-between items-center'>
                                 <Link to='/'>
-                                    <img className='w-[120px] h-[90px]' src="http://localhost:3000/images/logo.png" alt="" />
+                                    <img className='w-[120px] h-[90px]' src="https://frontend-lilac-nine.vercel.app/images/logo.png" alt="" />
                                 </Link>
                                 <div className='justify-center items-center w-[30px] h-[30px] bg-white text-slate-600
                                 border-slate-600 border rounded-sm cursor-pointer lg:hidden md-lg:flex xl:hidden hidden' onClick={() => setShowSidebar(false)}> 
@@ -144,13 +144,13 @@ const Header = () => {
                 <div className={`w-[300px] z-[999] transition-all duration-200 fixed ${showSidebar? '-left-[300px]' : 'left-0 top-0 '} overflow-y-auto bg-white h-screen py-6 px-8`}>
                     <div className='flex justify-start flex-col gap-6'>
                         <Link to='/'>
-                            <img src="http://localhost:3000/images/logo.png" alt="" />
+                            <img src="https://frontend-lilac-nine.vercel.app/images/logo.png" alt="" />
                         </Link>
                     </div>
                     <div className='flex justify-start items-center gap-10 '>
                         <div className='flex group cursor-pointer text-slate-600 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px]
                         after:bg-[#afafaf] after:-right-[16px] after:absolute'>
-                            <img src="http://localhost:3000/images/language.png" alt="" />
+                            <img src="https://frontend-lilac-nine.vercel.app/images/language.png" alt="" />
                             <span><IoMdArrowDropdown /></span>
                             <ul className='absolute invisible transition-all top-12 rounded-sm duration-200 text-white
                             p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10'>
