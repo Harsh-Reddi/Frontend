a7209d5f8a7ee61842ef9f313d2ea4cbc0c2af5b
Harsh-Reddi <Harshithareddi14@gmail.com>
2024-12-02 15:04:39 -0500

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const local = 'http://localhost:5000'
+const local = 'https://backend-91y0.onrender.com'
 const production = ''
 const api = axios.create({
     baseURL: `${local}/api`,
